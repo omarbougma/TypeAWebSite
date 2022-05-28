b185dccac3e39fd511e1791ba983de2672146cad
unknown <omarbgm4@gmail.com>
2022-05-28 15:45:31 +0100

    fixed donne sve

--- typea/src/main/java/com/projecttypea/typea/ws/DonéesProWS.java
+++ typea/src/main/java/com/projecttypea/typea/ws/DonéesProWS.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +44,7 @@ public class DonéesProWS {
         return donéesProService.findAll();
     }
 
-    @PostMapping("/user/adddonéespro")
+    @PutMapping("/user/adddonéespro")
     public int addDonesPro(@RequestBody DonéesPro donne, HttpSession session) {
         return donéesProService.addDonesPro(donne, session);
     }
